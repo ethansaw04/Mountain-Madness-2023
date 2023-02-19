880b74ecf3da52ffbdf589c36604dc92ba5f2bb5
Ethan Saw <ethansaw@gmail.com>
2023-02-18 16:58:26 -0800

    sending over for merging

--- mountain_rate/lib/screens/splash_screen.dart
+++ mountain_rate/lib/screens/splash_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:mountain_rate/screens/home_screen.dart';
+
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -9,16 +12,68 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   @override
-  Widget build(BuildContext context) {
-    double height = MediaQuery.of(context).size.height;
-    double width = MediaQuery.of(context).size.width;
+  // Widget build(BuildContext context) {
+  //   double height = MediaQuery.of(context).size.height;
+  //   double width = MediaQuery.of(context).size.width;
 
-    return Center(
-      child: SizedBox(
-        height: height,
-        width: width,
-        child:
-            Expanded(child: Image.asset("/home/evan/Downloads/mountain.jpg")),
+  //   return Center(
+  //     child: SizedBox(
+  //       height: height,
+  //       width: width,
+  //       child:
+  //           Expanded(child: Image.asset("/home/evan/Downloads/mountain.jpg")),
+  //     ),
+  //   );
+  // }
+
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTime();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: initScreen(context),
+    );
+  }
+  
+  startTime() async {
+    var duration = new Duration(seconds: 6);
+    return new Timer(duration, route);
+  }
+route() {
+    Navigator.pushReplacement(context, MaterialPageRoute(
+        builder: (context) => HomeScreen()
+      )
+    ); 
+  }
+  
+  initScreen(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            // Container(
+            //   child: Image.asset("images/logo.png"),
+            // ),
+             Padding(padding: EdgeInsets.only(top: 100.0)),
+            Text(
+              "Loading..",
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.blue
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 20.0)),
+            CircularProgressIndicator(
+              backgroundColor: Colors.white,
+              strokeWidth: 3,
+           )
+         ],
+       ),
       ),
     );
   }
